399653a94b5204ff74b0d937bd2a084679ee11b7
Joao Jacome <joao.jacome@smartbox.com>
2022-06-18 18:21:42 +0100

    .

--- src/GraphNavigator/DeserializationGraphNavigator.php
+++ src/GraphNavigator/DeserializationGraphNavigator.php
@@ -214,15 +214,12 @@ final class DeserializationGraphNavigator extends GraphNavigator implements Grap
                     $this->context->pushPropertyMetadata($propertyMetadata);
                     try {
                         $v = $this->visitor->visitProperty($propertyMetadata, $data);
+                        $this->accessor->setValue($object, $v, $propertyMetadata, $this->context);
                     } catch (NotAcceptableException $e) {
-                        if (false === $propertyMetadata->hasDefault) {
-                            throw $e;
+                        if (true === $propertyMetadata->hasDefault) {
+                            $this->accessor->setValue($object, $propertyMetadata->defaultValue, $propertyMetadata, $this->context);
                         }
-
-                        $v = $propertyMetadata->defaultValue;
                     }
-
-                    $this->accessor->setValue($object, $v, $propertyMetadata, $this->context);
 
                     $this->context->popPropertyMetadata();
                 }
